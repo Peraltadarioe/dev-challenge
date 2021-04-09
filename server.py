17b7b23fbe48b8cd241b1fcfea9616d83a7ce446
Dario Peralta <dario.peralta@claro.com.ar>
2021-04-09 19:07:23 -0300

    Se desarrolla la consulta para guardar el personaje cuando se consulta. Falta el calculo de rating

--- server.py
+++ server.py
@@ -3,16 +3,39 @@ from flask_cors import CORS
 import json
 from time import time
 import requests
+import sqlite3
 
-URL_SWAPI_DEV = "https://swapi.dev/api/people/1"
+
+URL_SWAPI_DEV = "https://swapi.dev/api/people/"
 # ------- Iniciacion de aplicacion Flask
 app = Flask(__name__)
 cors = CORS(app, resources={r"/gpon/*": {"origins": "*"}})
+con = sqlite3.connect('characters.db')
 
-@app.route("/character/", methods=['GET'])
-def get_character():
+
+# Create table
+try:
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE characters
+               (id integer, name text, height text, mass text, hair_color text, 
+               skin_color text, eye_color text, birth_year text, 
+               gender text, homeworld_name text, homeworld_population text, 
+               homeworld_know_residents_count integer, species_name text, 
+               avergae_rating real, max_rating integer)''')
+    con.close()
+except: # ver si la excepcion es porque ya eexite
+    pass
+
+@app.route("/character/<string:id>", methods=['GET'])
+def get_character(id):
     try:
-        response = requests.get(URL_SWAPI_DEV)
+        # recibir el id en los params porque el enunciado marca la url de la api sin id
+        # comprobar que sea un numero el id
+        # concatenar bien!
+        con = sqlite3.connect('characters.db')
+        cur = con.cursor()
+        url_api = URL_SWAPI_DEV + str(id)
+        response = requests.get(url_api)
         basic_info = response.json()
         
         home_world = requests.get(basic_info["homeworld"]).json()
@@ -29,6 +52,12 @@ def get_character():
         basic_info["homeworld"] = home_world_info
         basic_info["species_name"] = species_name
         print (basic_info)
+        data_character = (int(id), basic_info["name"], basic_info["height"], basic_info["mass"], basic_info["hair_color"], basic_info["skin_color"], basic_info["eye_color"],
+                            basic_info["birth_year"], basic_info["gender"], home_world_info["name"], home_world_info["population"], home_world_info["known_residents_counts"],
+                            species_name, 0, 0)
+        cur.execute('INSERT INTO characters VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', data_character)
+        con.commit()
+        con.close()
         return make_response(jsonify({'name': basic_info["name"]}), 200)
     except Exception as e:
         print (e)
