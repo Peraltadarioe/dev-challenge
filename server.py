6aff6eb991784a03436b7a26abb8892faae09b39
Dario Peralta <dario.peralta@claro.com.ar>
2021-04-11 19:43:28 -0300

    Se completan los dos endpoints

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
-from flask import Flask, request, jsonify, abort,make_response,send_file,render_template
+# ------- START IMPORTS BLOCK -------------------------------------------------------------------
+from flask import Flask, request, jsonify,make_response
 from flask_cors import CORS
 import json
 from time import time
 import requests
 import sqlite3
+# ------- END IMPORTS BLOCK -------------------------------------------------------------------
 
 
+# VARS -----------------------------------------------------------------
 URL_SWAPI_DEV = "https://swapi.dev/api/people/"
-# ------- Iniciacion de aplicacion Flask
+# VARS -----------------------------------------------------------------
+
+# ------ START INIT BLOCK ----------------------------------------------
 app = Flask(__name__)
 cors = CORS(app, resources={r"/gpon/*": {"origins": "*"}})
 con = sqlite3.connect('characters.db')
+# ------ END INIT BLOCK ----------------------------------------------
 
 
-# Create table
+# ------ CREATE TABLE ------------------------
 try:
     cur = con.cursor()
     cur.execute('''CREATE TABLE ratings
@@ -21,7 +27,14 @@ try:
     con.close()
 except: # ver si la excepcion es porque ya eexite
     pass
+# ------ CREATE TABLE ------------------------
 
+
+# -------------------------------------- START API ENDPOINTS ---------------------------------
+
+# Recibe el id del character y devuelve los datos requeridos
+# Para ello consulta a la api y calcula los ratings guardados
+# Si no existen ratings para el personaje devuelve null en los campos average y max
 @app.route("/character/<string:id>", methods=['GET'])
 def get_character(id):
     try:
@@ -30,16 +43,16 @@ def get_character(id):
         # concatenar bien!
         
         url_api = URL_SWAPI_DEV + str(id)
-        response = requests.get(url_api)
+        response = requests.get(url_api, verify=False)
         basic_info = response.json()
         
-        home_world = requests.get(basic_info["homeworld"]).json()
+        home_world = requests.get(basic_info["homeworld"], verify=False).json()
         home_world_info = { "name": home_world["name"], 
                             "population": home_world["population"], 
                             "known_residents_counts": len(home_world["residents"])
                           }
         if len (basic_info["species"]):
-            species = requests.get(basic_info["species"][0]).json()
+            species = requests.get(basic_info["species"][0], verify=False).json()
             species_name = species["name"]
         else:
             species_name = ""
@@ -57,46 +70,40 @@ def get_character(id):
             average_rating = sum(ratings) / len(ratings)
             max_rating = max(ratings)
         
-        basic_info["homeworld"] = home_world_info
-        basic_info["species_name"] = species_name
-        data_response = {
-            "name": basic_info["name"],
-            "height": basic_info["height"],
-            "mass": basic_info["mass"],
-            "hair_color": basic_info["hair_color"],
-            "skin_color": basic_info["skin_color"],
-            "eye_color": basic_info["eye_color"],
-            "birth_year": basic_info["birth_year"],
-            "gender": basic_info["gender"],
-            "homeworld": home_world_info,
-            "species_name": species_name,
-            "average_rating": average_rating,
-            "max_rating": max_rating
+        data_response= {"name": basic_info["name"], "height": basic_info["height"], "mass": basic_info["mass"],
+                        "hair_color": basic_info["hair_color"], "skin_color": basic_info["skin_color"], "eye_color": basic_info["eye_color"],
+                        "birth_year": basic_info["birth_year"], "gender": basic_info["gender"], "homeworld": home_world_info,
+                        "species_name": species_name, "average_rating": average_rating, "max_rating": max_rating
         }
         return make_response(jsonify(data_response), 200)
     except Exception as e:
         print (e)
-        return make_response(jsonify({'error': 'Not found'}), 404)
+        return make_response(jsonify({'state': 'Internal error'}), 500)
 
+# Ruta que recibe el id del personaje y un rating
+# Se guarda en la base de datos
 @app.route("/character/rating/", methods=['POST'])
 def set_character_rating ():
     data = request.json
-    print(data)
-    con = sqlite3.connect('characters.db')
-    cur = con.cursor()
-    cur.execute('INSERT INTO ratings VALUES (?,?)', (data["id"], data["rating"]))
-    con.commit()
-    con.close()
-    return make_response(jsonify({"state": "ok"}), 200)
-    
-    pass
+    try:
+        con = sqlite3.connect('characters.db')
+        cur = con.cursor()
+        cur.execute('INSERT da INTO ratings VALUES (?,?)', (data["id"], data["rating"]))
+        con.commit()
+        con.close()
+        return make_response(jsonify({"state": "ok"}), 200)
+    except:
+        return make_response(jsonify({"state": "Error al intentar guardar el rating"}), 500)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    # -------------------------------------- END API ENDPOINTS ---------------------------------
+
 
 # -------------------------------------- START ERROR HANDLES ROUTES BLOCK ---------------------------------
-#----------------------------------------------------------------------------------------------------------
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'state': 'Not found'}), 404)
+# -------------------------------------- END ERROR HANDLES ROUTES BLOCK ---------------------------------
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
